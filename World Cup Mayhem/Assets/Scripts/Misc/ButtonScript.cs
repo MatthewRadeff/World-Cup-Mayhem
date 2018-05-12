597554f0999d6e3cba166f6032dd2307786484fa
MatthewRadeff <matt_radeff@hotmail.com>
2018-05-12 16:05:57 -0400

    Scene Flow
    All the scenes that are wanted to be present and are layed out ready for functionality. Can start making the mechanics of the game.

---
+++ World Cup Mayhem/Assets/Scripts/Misc/ButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+}
